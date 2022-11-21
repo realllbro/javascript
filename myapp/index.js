5a74d039ec8f4899cb386f63c9dd0e3a0ab4fa6c
realllbro <xxbrox@naver.com>
2022-11-22 00:28:23 +0900

    22

--- myapp/index.js
+++ myapp/index.js
@@ -1,2 +1,10 @@
 const arr = [1,2,3];
-arr.forEach(alert);
+//arr.forEach(alert);
+
+
+let d = new Date();
+console.log(d.getMonth());
+console.log(d.getFullYear());
+console.log(d.getDay());
+//console.log(d.getWeek());
+console.log(new Date().getWeek()); 
